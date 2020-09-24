a6515f206cd2df6ed15489ad30f7b124de215db1
GgoldGodwyn <35366224+GgoldGodwyn@users.noreply.github.com>
2020-09-24 03:36:48 -0700

    Update chit_chat.ino
    added comments to line of code for better understanding

--- arduino_Esp8266/chit_chat/chit_chat.ino
+++ arduino_Esp8266/chit_chat/chit_chat.ino
@@ -17,13 +17,13 @@ char question[50];
 char userpassword[15];
 
 int  authenticateUser(){
-  int v=0;
+  int v=0; // initialise a variable to be used as a flag
                 Serial.println("Please enter password");
   while(1){
                   delay(200);
 //  Serial.print('}');
         while(Serial.available()){
-          c=Serial.read();
+          c=Serial.read();   // capture incoming serial data
 //   Serial.print(c);
           if(c=='\n' || c =='\r'){
             v=1;
@@ -32,29 +32,29 @@ int  authenticateUser(){
           c=Serial.read(); // waste other incomming eg: /r/n
             }
             else{
-              sprintf(userpassword,"%s%c",userpassword,c);
+              sprintf(userpassword,"%s%c",userpassword,c);  // patch incoming serial data to recent ones
 //   Serial.println(userpassword);
-              if((strlen (userpassword)) >13){
-                memset(userpassword,'\0',sizeof userpassword);
+              if((strlen (userpassword)) >13){ // password must not be greater than 13
+                memset(userpassword,'\0',sizeof userpassword); //clear the data
                 Serial.println("Wrong password!! try again");
                 
                 while(Serial.available()){
-                c=Serial.read();
-                delay(200);
+                c=Serial.read(); //wipe all junck coming in
+                delay(200); // delay so all junck can come in and be wiped
                 }
                       return 0;
               }
             }
             if(v==1){
-              if((strcmp(userpassword,"degree360"))==0){
-                Serial.println("Login sucessful");
-                sprintf(Data1,"%s%s%s",Data1,userpassword,"\",\"word\" : \"");
+              if((strcmp(userpassword,"degree360"))==0){ // if password is correct
+                Serial.println("Login sucessful");  // output "success" to serial monitor
+                sprintf(Data1,"%s%s%s",Data1,userpassword,"\",\"word\" : \"");  // patch incoming serial data to recent ones
 //    Serial.println(Data1);
                 return 1;
               }
               else{
               v=0;
-                memset(userpassword,'\0',sizeof userpassword);
+                memset(userpassword,'\0',sizeof userpassword);  //clear the data
                 Serial.println("wrong password!! try again");
                 while(Serial.available()){
                 c=Serial.read();
@@ -88,7 +88,7 @@ void setup() {
 
   
   Serial.println("Now you can chat with ChitChat\n");
-  memset(question,'\0',sizeof question);
+  memset(question,'\0',sizeof question);  //clear the data
 }
 
 void loop() {
@@ -104,14 +104,14 @@ void loop() {
         c=Serial.read(); // waste other incomming eg: /r/n
           }
           else{
-            sprintf(question,"%s%c",question,c);
+            sprintf(question,"%s%c",question,c);  // patch incoming serial data to recent ones
           }
       }
       if(go==1){
         go=0;
-        sprintf(question,"%s%s",question,"\"}");
+        sprintf(question,"%s%s",question,"\"}");  // patch incoming serial data to recent ones
 //    Serial.println(question);
-        sprintf(Data,"%s%s",Data1,question);
+        sprintf(Data,"%s%s",Data1,question);  // patch incoming serial data to recent ones
         //Serial.println(Data);
      String response = PostRequest(url, Data);
 //    Serial.println(response);
@@ -144,8 +144,8 @@ void loop() {
   }
         }
   }
-  memset(question,'\0',sizeof question);
-  memset(Data,'\0',sizeof Data);
+  memset(question,'\0',sizeof question);  //clear the data
+  memset(Data,'\0',sizeof Data);  //clear the data
     }
      ///////////////
     }
@@ -155,7 +155,13 @@ void loop() {
   
 }
 
+/*
+the below function take two parameter, 
+uses the "dURL" to connect to the API
+and "jsonData" as a json to post the request.
 
+ it respond with a string response from the server
+*/
 
 String PostRequest(const char* dURL, const char* jsonData) { 
   HTTPClient http;
@@ -165,17 +171,17 @@ String PostRequest(const char* dURL, const char* jsonData) {
   
       http.addHeader("Content-Type", "application/json"); //content-type header type
   // Send HTTP POST request
-  int ResponseCode = http.POST(jsonData);
+  int ResponseCode = http.POST(jsonData); // post the data
   
-  String feedback = "{}"; 
+  String feedback = "{}";  //init string to hold response from the server
   
   if (ResponseCode>0) {
-  if (ResponseCode>=200 && ResponseCode<=201) {
+  if (ResponseCode>=200 && ResponseCode<=201) { // if http Response code is 200 or 201, it must have got some good response
    // Serial.print("HTTP Response code: ");
    // Serial.println(ResponseCode);
-    feedback = http.getString();
+    feedback = http.getString(); //append response to feedback
     }
-  else {
+  else { // else, something must have gone wrong
     Serial.print("Http Response code : ");
     Serial.println(ResponseCode);
   }
@@ -187,7 +193,7 @@ String PostRequest(const char* dURL, const char* jsonData) {
   
         
       // Free resources
-      http.end();
+      http.end(); // end the session
       
-    return feedback;
+    return feedback; // return the feedback
   }
